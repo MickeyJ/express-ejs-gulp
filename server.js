9a1e5438d927b87b856a78eea783fd9050c40873
Mickey <develop.agility@gmail.com>
2016-02-23 12:38:18 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+/* dependencies */
+var express = require('express');
+var ejs = require('ejs');
+
+
+/* Creat App */
+var app = express();
+
+
+/* Setup Views */
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+
+/* Home Page */
+app.get('/', function(req, res, next){
+  res.render('index', {
+    message: 'This is a home page'
+  })
+});
+
+/* About Page */
+app.get('/about', function(req, res, next){
+  res.render('about', {
+    message: 'This is an about page'
+  })
+});
+
+
+/* Create Server */
+var port = 9000;
+app.listen(port, function(){
+  console.log('listening on port: '+ port)
+});
